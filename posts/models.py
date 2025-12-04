aad5e94e19d55becbb0a0d94292e831ab7f96c57
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-05 05:46:41 +0600

    Add Category model and link to Post model

--- posts/models.py
+++ posts/models.py
@@ -4,10 +4,17 @@ from django.conf import settings
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+
 class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_imgs/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
