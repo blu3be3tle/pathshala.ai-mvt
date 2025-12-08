2de554d4b372ff759addeb372ad03c1a13068b16
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-08 16:37:15 +0600

    Update URL patterns to set posts home view as the root path

--- pathshala_ai/urls.py
+++ pathshala_ai/urls.py
@@ -4,5 +4,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
+    path('', include('posts.urls'))
 ] + debug_toolbar_urls()
