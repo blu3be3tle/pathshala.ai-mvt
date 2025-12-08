6502436e9063036d40b20ff2d8a1acf8675e8230
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-08 19:17:31 +0600

    Add related_name to category field in Post model for better reverse lookup

--- posts/models.py
+++ posts/models.py
@@ -13,8 +13,8 @@ class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='1')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='posts', default='1')
     content = models.TextField()
-    image = models.ImageField(upload_to='post_imgs/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
