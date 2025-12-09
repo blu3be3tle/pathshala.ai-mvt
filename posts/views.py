9c88d1f43295936b273f1a3e2dbcb702b18a0420
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-09 19:01:31 +0600

    Add login_required decorator to post_create view and fix template path

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -14,10 +15,11 @@ def post_detail(request, pk):
     return render(request, "posts/post_detail.html", {"post": post})
 
 
-def post_create(request, pk):
+@login_required
+def post_create(request):
     if request.method == "POST":
         content = request.POST.get("content")
         Post.objects.create(user=request.user, content=content)
         return redirect("post_list")
 
-    return render(request, "posts/posts_create.html")
+    return render(request, "posts/post_create.html")
