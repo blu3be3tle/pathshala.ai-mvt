a1a7cff2498a69a61712334efcd56aeecf9471a4
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-09 17:06:47 +0600

    Refactor views: add post_list, post_detail, and post_create functions for handling posts

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html', {'msg': 'PathshalaAI is running!'})
+def post_list(request):
+    post = Post.object.select_related("user").all().order_by("-created_at")
+    return render(request, "posts/post_list.html", {"post": post})
+
+
+def post_detail(request, pk):
+    post = Post.object.get(id=pk)
+    return render(request, "posts/post_detail.html", {"post": post})
+
+
+def post_create(request, pk):
+    if request.method == "POST":
+        content = request.POST.get("content")
+        Post.objects.create(user=request.user, content=content)
+        return redirect("post_list")
+
+    return render(request, "posts/posts_create.html")
