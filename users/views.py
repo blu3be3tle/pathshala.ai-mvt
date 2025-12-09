60a237306d1dff36253b55ae17dcf47178bb1b13
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-09 19:20:24 +0600

    Implement signup view for user registration and redirect to post list

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib.auth import login
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        login(request, user)
+        return redirect("post_list")
+
+    return render(request, "users/signup.html")
+
