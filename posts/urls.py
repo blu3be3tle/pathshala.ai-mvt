97dc9d254440783a297a7b73141be725576b59bb
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-14 16:56:14 +0600

    post like link created

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_create, post_detail, post_list, post_delete, post_edit
+from .views import post_create, post_detail, post_list, post_delete, post_edit, toggle_like
 
 urlpatterns = [
     path('', post_list, name="post_list"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>', post_detail, name="post_detail"),
     path('post/<int:pk>/edit/', post_edit, name="post_edit"),
     path('post/<int:pk>/delete/', post_delete, name="post_delete"),
+    path('post/<int:pk>/like/', toggle_like, name="toggle_like")
 ]
