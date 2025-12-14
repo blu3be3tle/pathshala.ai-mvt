d3a832a021efd5c5903827463907be21e48b3ff7
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-12-14 08:51:13 +0600

    alter like unique together

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-
 class Post(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE
@@ -25,3 +24,6 @@ class Like(models.Model):
         related_name='likes'
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
